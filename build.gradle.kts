242fa532a16b807b80051d8355a808de678b7c92
tinashe <tinashe@opki.io>
2026-01-12 12:44:11 +0200

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.apache.commons:commons-compress:1.28.0")
+
+    implementation("org.apache.commons:commons-configuration2:2.13.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
